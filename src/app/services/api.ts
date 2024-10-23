9167bc9e246def36bf89005ab99cde6fb1a93f4c
k1ego <ladonin05@mail.ru>
2024-10-23 03:34:38 +0300

    Added userApi

---
+++ src/app/services/api.ts
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react";
+import { BASE_URL } from "../../constants";
+import { RootState } from "../store";
+
+const baseQuery = fetchBaseQuery({
+	baseUrl: `${BASE_URL}/api`,
+	// prepareHeaders: (headers, { getState }) => {
+	// 	const token = (getState() as RootState).auth.token || localStorage.getItem("token");
+	// 	if (token) {
+	// 		headers.set("authorization", `Bearer ${token}`);
+	// 	}
+	// 	return headers;
+	// },
+});
+
+const baseQueryWithReauth = retry(baseQuery, { maxRetries: 1 })
+
+export const api = createApi({
+	reducerPath: "splitApi",
+	baseQuery: baseQueryWithReauth,
+	// автоматически повторно запрашивает данные при монтировании компонента или изменении аргументов
+	refetchOnMountOrArgChange: true,
+	endpoints: () => ({})
+})
---
+++ src/app/services/userApi.ts
@@ -0,0 +1,59 @@
+import { User } from '../types';
+import { api } from './api';
+
+export const userApi = api.injectEndpoints({
+	endpoints: builder => ({
+		login: builder.mutation<
+			// ожидает два аргумента типа: тип данных ответа и тип данных параметров запроса.
+			{ token: string },
+			{ email: string; password: string }
+		>({
+			query: userData => ({
+				url: '/login',
+				method: 'POST',
+				body: userData,
+			}),
+		}),
+		register: builder.mutation<
+			{ email: string; password: string; name: string },
+			{ email: string; password: string; name: string }
+		>({
+			query: userData => ({
+				url: '/register',
+				method: 'POST',
+				body: userData,
+			}),
+		}),
+		current: builder.query<User, void>({
+			query: () => ({
+				url: '/current',
+				method: 'GET',
+			}),
+		}),
+		getUserById: builder.query<User, string>({
+			query: id => ({
+				url: `/user/${id}`,
+				method: 'GET',
+			}),
+		}),
+		updateUser: builder.mutation<User, { userData: FormData; id: string }>({
+			query: ({ userData, id }) => ({
+				url: `/user/${id}`,
+				method: 'PUT',
+				body: userData,
+			}),
+		}),
+	}),
+});
+
+export const {
+	useLoginMutation,
+	useRegisterMutation,
+	useCurrentQuery,
+	useGetUserByIdQuery,
+	useUpdateUserMutation,
+} = userApi;
+
+export const {
+	endpoints: { login, register, current, getUserById, updateUser },
+} = userApi;
