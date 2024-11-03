537517ed8f4d3bf1ff349eee7a3323a00ee1bf67
k1ego <ladonin05@mail.ru>
2024-11-03 20:02:46 +0300

    Руализована логика отображения постов у всех пользователей

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-import {Card as NextUiCard} from '@nextui-org/react'
+import {
+	CardBody,
+	CardFooter,
+	CardHeader,
+	Card as NextUiCard,
+	Spinner,
+} from '@nextui-org/react';
+import { FcDislike } from 'react-icons/fc';
+import { MdOutlineFavoriteBorder } from 'react-icons/md';
+import { RiDeleteBinLine } from 'react-icons/ri';
+import { useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDeleteCommentMutation } from '../../app/services/commentsApi';
+import {
+	useLikePostMutation,
+	useUnlikePostMutation,
+} from '../../app/services/likesApi';
+import {
+	useDeletePostMutation,
+	useLazyGetAllPostsQuery,
+	useLazyGetPostByIdQuery,
+} from '../../app/services/postsApi';
+import { selectCurrentUser } from '../../features/user/userSlice';
+import { formatToClientDate } from '../../utils/format-to-client-date';
+import { MetaInfo } from '../meta-info';
+import { Typography } from '../typography';
+import { User } from '../user';
+import { FaRegComment } from 'react-icons/fa';
+import { ErrorMessage } from '../error-message';
 
 type Props = {
-	avatarUrl: string
+	avatarUrl: string;
 	name: string;
 	authorId: string;
 	content: string;
@@ -14,22 +42,75 @@ type Props = {
 	id?: string;
 	cardFor: 'comment' | 'post' | 'currentPost';
 	likedByUser?: boolean;
-}
+};
 
 export const Card: React.FC<Props> = ({
-	avatarUrl = "",
-	name = "",
-	authorId = "",
-	content = "",
-	commentId = "",
+	avatarUrl = '',
+	name = '',
+	authorId = '',
+	content = '',
+	commentId = '',
 	likeCount = 0,
 	commentsCount = 0,
-	createdAt: Date,
+	createdAt,
 	id = '',
 	cardFor = 'post',
 	likedByUser = false,
 }) => {
+	const [likePost] = useLikePostMutation();
+	const [unlikePost] = useUnlikePostMutation();
+	const [triggerGetAllPosts] = useLazyGetAllPostsQuery();
+	const [triggerGetPostById] = useLazyGetPostByIdQuery();
+	const [deletePost, deletePostStatus] = useDeletePostMutation();
+	const [deleteComment, deleteCommentStatus] = useDeleteCommentMutation();
+	const [error, setError] = useState('');
+	const navigate = useNavigate();
+	const currentUser = useSelector(selectCurrentUser);
+
 	return (
-		<div>Card</div>
-	)
-}
+		<NextUiCard className='mb-5'>
+			<CardHeader className='justify-between items-center bg-transparent'>
+				<Link to={`/users/${authorId}`}>
+					<User
+						name={name}
+						className='text-small font-semibold leading-non text-default-600'
+						avatarUrl={avatarUrl}
+						description={
+							createdAt && formatToClientDate(createdAt.toLocaleString())
+						}
+					/>
+				</Link>
+				{authorId === currentUser?.id && (
+					<div className='cursor-pointer'>
+						{deletePostStatus.isLoading || deleteCommentStatus.isLoading ? (
+							<Spinner />
+						) : (
+							<RiDeleteBinLine />
+						)}
+					</div>
+				)}
+			</CardHeader>
+			<CardBody className='px-3 py-2 mb-5'>
+				<Typography>{content}</Typography>
+			</CardBody>
+			{cardFor !== 'comment' && (
+				<CardFooter className='gap-3'>
+					<div className='flex gap-5 items-center'>
+						<div>
+							<MetaInfo
+								count={likeCount}
+								Icon={likedByUser ? FcDislike : MdOutlineFavoriteBorder}
+							/>
+						</div>
+						<Link
+							to={`/posts/${id}`}
+							className='cursor-pointer'>
+								<MetaInfo count={commentsCount} Icon={FaRegComment} />
+							</Link>
+					</div>
+					<ErrorMessage error={error} />
+				</CardFooter>
+			)}
+		</NextUiCard>
+	);
+};
