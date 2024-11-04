a182e40e10c1d299c66afa2b265d8d0b634042a6
k1ego <ladonin05@mail.ru>
2024-11-04 21:08:49 +0300

    Фиксанут небольшой баг в current-post при нажатии на лайк

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -90,7 +90,7 @@ export const Card: React.FC<Props> = ({
 			likedByUser
 				? await unlikePost(id).unwrap()
 				: await likePost({ postId: id }).unwrap();
-				await refetchPosts();
+				await triggerGetPostById(id).unwrap();
 		} catch (error) {
 			if (hasErrorField(error)) {
 				setError(error.data.error);
